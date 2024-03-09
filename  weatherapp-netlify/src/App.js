4fe44c07d7ffeaa3ef3a0c55a47bbe962963ee62
Nisanth Selvaraj <121391945+Nisanth2004@users.noreply.github.com>
2024-03-09 14:43:55 +0530

    weather app using react

---
+++  weatherapp-netlify/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import WeatherApp from './weatherapp/WeatherApp';
+
+function App() {
+  return (
+    <div className="App">
+    <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
